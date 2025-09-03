c9234bd737cb169fa7f07937807a8270f5a161ea
PavelBoyko-beep <74496407+PavelBoyko-beep@users.noreply.github.com>
2025-09-03 19:14:01 +0300

    Update 01_schema.sql

--- week01_sql/sql/01_schema.sql
+++ week01_sql/sql/01_schema.sql
@@ -1,1 +1,14 @@
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY,
+    country TEXT,
+    signup_date DATE
+);
 
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT REFERENCES customers(customer_id),
+    order_date DATE,
+    channel TEXT,           -- 'web' / 'mobile' / 'ads'
+    amount NUMERIC(10,2),   -- сумма заказа $
+    status TEXT             -- 'paid' / 'refunded' / 'pending'
+);
